6b5b5f72db7675551e64f573d45c136585e22c75
Crispina Muriel <47477912+crispinamuriel@users.noreply.github.com>
2020-01-24 17:11:55 -0500

    Algo completed
    hackerrank q

--- algos.js
+++ algos.js
@@ -20,16 +20,54 @@
 // }
 //Method 2
 
-function doubleForLoop(arr) {
-  let memo = {};
-  for(let i = 0; i < arr.length; i++ ) {
-    for(let j = 0; j < arr.length; j++) {
-      if(arr[i] === arr[j] && i !== j && !memo[arr[i]]) memo[arr[i]] = true;
-    }
-  }
-  return Object.keys(memo).map(key => Number(key));
+// function doubleForLoop(arr) {
+//   let memo = {};
+//   for(let i = 0; i < arr.length; i++ ) {
+//     for(let j = 0; j < arr.length; j++) {
+//       if(arr[i] === arr[j] && i !== j && !memo[arr[i]]) memo[arr[i]] = true;
+//     }
+//   }
+//   return Object.keys(memo).map(key => Number(key));
   
+// }
+
+
+// doubleForLoop(arr);
+
+/*Hackerrank interview prep: Function Description
+
+Complete the repeatedString function in the editor below. It should return an integer representing the number of occurrences of a in the prefix of length  in the infinitely repeating string.
+
+repeatedString has the following parameter(s):
+
+s: a string to repeat
+n: the number of characters to consider */
+
+// Complete the repeatedString function below.
+function repeatedString(s, n) {
+    if(n < s.length) {
+        let count = 0;
+        for(let i = 0; i < n; i++){
+         if(s[i] === 'a')count++;
+        }
+        return count;
+    }
+    
+    let count = 0;
+    
+    for(let i = 0; i < s.length; i++){
+        if(s[i] === 'a') count++;
+    }
+
+    let repeat = Math.floor(n/s.length);
+
+    let remainder = n % repeat;
+    count *= repeat;
+    console.log(remainder);
+
+    for(let i=0; i<remainder; i++){
+        if(s[i]=== 'a')count++
+    }
+    return count;
+
 }
-
-
-doubleForLoop(arr);
