75001f2ad7cfb48c5d6d0fca0b8207becbabf32a
Crispina Muriel <47477912+crispinamuriel@users.noreply.github.com>
2020-03-15 23:01:38 -0400

    Update hackerrank.js

--- hackerrank.js
+++ hackerrank.js
@@ -73,3 +73,16 @@ function birthdayCakeCandles(ar) {
     }
     return candles;
 }
+//Two Sum
+var twoSum = function(nums, target) {
+    const hashMap = new Map();
+    
+    for(let i = 0; i < nums.length; i++) {
+        let value = nums[i];
+        let compliment = target - value;
+        if(hashMap.has(compliment)) {
+            return [hashMap.get(compliment), i]
+        }
+        hashMap.set(value, i);
+    }
+};
