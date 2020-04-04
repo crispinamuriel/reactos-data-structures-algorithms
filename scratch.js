151a4da87210b1097e2ef54de119a3b90948be63
Crispina Muriel <47477912+crispinamuriel@users.noreply.github.com>
2020-04-04 09:15:46 -0400

    Create scratch.js

---
+++ scratch.js
@@ -0,0 +1,13 @@
+const useStyles = makeStyles({
+  appCalendar: {
+    margin: '0 auto',
+    maxWidth: '50%',
+    maxheight: '30%',
+    backgroundColor: '#f64141',
+    color: 'white',
+  },
+  today: {
+    color: 'white',
+    backgroundColor: '#f64141',
+  },
+});
