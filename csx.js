8ac55ad482fcd68ffdf6c35f9d5ee3a7940371da
Crispina Muriel <47477912+crispinamuriel@users.noreply.github.com>
2020-04-28 10:12:00 -0400

    Update csx.js

--- csx.js
+++ csx.js
@@ -99,3 +99,54 @@ console.log(quarantineSchedule('wednesday')) // should log 'Puzzles'
 console.log(quarantineSchedule('FRIDAY')) // should log 'Netflix'
 console.log(quarantineSchedule('SuNDay')) // should log 'Exercise'
 console.log(quarantineSchedule('tuesDAY')) // should log 'Code'
+
+const trashDuty = (exerciseMins, exerciseChecker) => {
+  let resultArr = [];
+  for(let key in exerciseMins) {
+    if(exerciseChecker(exerciseMins[key])) {
+      resultArr.push(key);
+    }
+  }
+  return resultArr;
+};
+
+const exerciseChecker = (arr) => {
+  let days = 0;
+  arr.forEach((day) => {
+    if (day >= 30) {
+      days++;
+    }
+  });
+  return days > 3;
+}
+
+
+const exerciseMins = {
+    'David': [25, 35, 15, 60, 10, 0, 60],
+    'Ryan': [0, 20, 60, 85, 30, 45, 45],
+    'Harry': [15, 25, 40, 60, 60, 90, 0],
+    'Mike': [60, 80, 25, 45, 10, 60, 30],
+    'Chris': [30, 30, 45, 90, 90, 15, 0]
+};
+
+const exerciseMins2 = {
+    'David': [50, 35, 15, 60, 10, 35, 60],
+    'Ryan': [0, 20, 60, 85, 30, 45, 45],
+    'Harry': [15, 25, 40, 0, 60, 90, 0],
+    'Mike': [60, 80, 25, 45, 10, 60, 30],
+    'Chris': [30, 30, 45, 90, 90, 15, 0]
+};
+
+const exerciseMins3 = {
+    'David': [50, 35, 15, 60, 10, 35, 60],
+    'Ryan': [0, 20, 60, 85, 30, 45, 45],
+    'Harry': [40, 25, 40, 80, 60, 90, 0],
+    'Mike': [60, 80, 25, 45, 10, 60, 30],
+    'Chris': [30, 10, 45, 0, 90, 15, 0]
+};
+
+
+// UNCOMMENT THIS TO TEST YOUR CODE
+console.log(trashDuty(exerciseMins, exerciseChecker)); // should log ['Ryan', 'Harry', 'Mike', 'Chris'];
+console.log(trashDuty(exerciseMins2, exerciseChecker)) // should log ['David', 'Ryan', 'Mike', 'Chris'];
+console.log(trashDuty(exerciseMins3, exerciseChecker)
