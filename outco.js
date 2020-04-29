78e14a2bc76a3ced0366e025d8c22caa3ec1531e
Crispina Muriel <47477912+crispinamuriel@users.noreply.github.com>
2020-04-28 22:29:02 -0400

    Update outco.js

--- outco.js
+++ outco.js
@@ -1,2 +1,3 @@
 https://github.com/MaximAbramchuck/awesome-interview-questions
 https://github.com/h5bp/Front-end-Developer-Interview-Questions
+https://docs.google.com/document/d/1e5qUpP8UTl7WloWkaZZklZYCtFa0LT9-Y1mBnzRl9Og/edit?usp=sharing
